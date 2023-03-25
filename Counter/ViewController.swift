fe7f68f866fc321bbf91b5ba4b25123f05cf849e
Dmitriy <dima.igrbch@gmail.com>
2023-03-25 21:51:35 +0300

    Добавлен счётчик при нажатии на кнопку

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var countLabel = 0
+    
+    @IBOutlet weak var textLAbel: UILabel!
+    
+    
+    @IBAction func tapButton(_ sender: Any) {
+        countLabel += 1
+        textLAbel.text = "\(countLabel)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textLAbel.text = "0"
+        
+        
+        
+        
     }
 
 
